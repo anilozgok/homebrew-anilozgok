c1191f8c3b19c08f18023e5f3f47c1d87b6c47d4
Anıl Özgök <anil.ozgok@trendyol.com>
2023-07-17 15:18:22 +0300

    exchange-cli-example formula

---
+++ exchange.rb
@@ -0,0 +1,15 @@
+class Exchange < Formula
+  desc "exchance cli tool"
+  homepage "https://github.com/anilozgok/exchange-cli-example"
+  url "https://github.com/anilozgok/exchange-cli-example/releases/download/v1.1.0/exchange-cli-example_1.1.0_darwin_arm64.tar.gz"
+  sha256 "bbcad2e81bc3c5cd35ffdcfb873670e7042fe9dbfcdc113899410594694622af"
+  license "MIT"
+  
+  def install
+    bin.install "exchange-cli-example"
+  end
+
+  test do
+    system "#{bin}/exchange-cli-example"
+  end
+end
